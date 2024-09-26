d5bb5dda5b733c90e3af551ad542d38172b1efa6
Aparna1407 <aparnadesai7@gmail.com>
2024-09-26 17:21:20 +0530

    c113

---
+++ MathOperations.py
@@ -0,0 +1,13 @@
+import math  
+# floor and ceil methods
+print("The Floor and Ceiling value of 23.56 are:"+str(math.ceil(23.56))+","+str(math.floor(23.56)))
+
+x=10
+y=-15
+#copysign function
+print("The Value of x after copying the sign from y is:"+str(math.copysign(x,y)))
+
+#using fabs and gcd function
+print("Absolute value of -96 and 56 are :"+str(math.fabs(196))+","+str(math.fabs(56)))
+
+print("The GCD of 24 and 56:"+str(math.gcd(24)))
